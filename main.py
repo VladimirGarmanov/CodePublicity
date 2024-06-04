974a0814e91bf53cd19731b9f11ab9bae183da4a
vladimir <108998854+VladimirGarmanov@users.noreply.github.com>
2024-06-04 11:33:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,77 @@ import asyncio
 import sqlite3
 from aiogram import Bot, Dispatcher, types, executor
 import openai
+import re
+client = openai.OpenAI(api_key="sk-proj-dMxBPSgPFHxHooKvFEMmT3BlbkFJDEuEtpmEZMkxJbHrwvvk")
+from aiogram.types import ChatActions
+users = []
+from openpyxl import Workbook, load_workbook
 
-client = openai.OpenAI(api_key="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")
+file1 = client.files.create(
+  file=open("ТРЕБОВАНИЯ К СПИКЕРАМ.pdf", "rb"),
+  purpose='assistants'
+)
+file2 = client.files.create(
+  file=open("Основные_правила.txt", "rb"),
+  purpose='assistants'
+)
+file3 = client.files.create(
+  file=open("Ценности_сообщества_Код_публичности.docx", "rb"),
+  purpose='assistants'
+)
 
-Assistant_ID = 'Assistant_id'
-TELEGRAM_TOKEN = '6787450167:AAGByC55w7mBxObi0hK7XWh29NOqgBMTqEs'
+
+Assistant_ID = 'asst_mYsdSqR87HqRc6hkmVIuU01x'
+print(Assistant_ID)
+TELEGRAM_TOKEN = '6832211064:AAGj7DTV_LqZSli9Xz2Yn-0g4oegrg_4aFM'
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
+threads = {}
 
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-    chat_id INTEGER PRIMARY KEY,
-    thread TEXT
-)''')
-conn.commit()
+async def remove_text_in_brackets(text):
+    # Используем регулярное выражение для поиска текста в квадратных скобках и удаляем его
+    return re.sub(r'\[.*?\]', '', text)
+async def write_to_excel(file_name, values):
+    try:
+        # Пытаемся загрузить существующую книгу Excel
+        wb = load_workbook(file_name)
+    except FileNotFoundError:
+        # Если файл не найден, создаем новую книгу
+        wb = Workbook()
 
+    # Выбираем активный лист или создаем новый, если его нет
+    ws = wb.active
 
+    # Записываем значения в новую строку
+    ws.append(values)
+
+    # Сохраняем книгу
+    wb.save(file_name)
+values_to_write = ['дата', 'ник', 'вопрос', 'ответ']  # Значения для записи
+write_to_excel("output.xlsx", values_to_write)
+
+@dp.message_handler(commands=['send'])
+async def send_excel_file(message: types.Message):
+    file_name = "output.xlsx"
+    with open(file_name, 'rb') as file:
+        await bot.send_document(message.chat.id, file)
 # Функция для добавления пользователя в базу данных
 async def handle_with_assistant(message, chat_id):
+    await bot.send_chat_action(message.chat.id, ChatActions.TYPING)
     print('генерация началась')
-    cursor.execute('SELECT thread FROM users WHERE chat_id = ?', (chat_id,))
-    result = cursor.fetchone()
-    thread_id = result[0] if result is not None else add_user(chat_id)
+
+    thread_id = threads[chat_id]
     message_answer = client.beta.threads.messages.create(
         thread_id=thread_id,
         role="user",
-        content=message.text
+        content=f"{message.text}"
 
     )
     run = client.beta.threads.runs.create(
         thread_id=thread_id,
         assistant_id=Assistant_ID,
-        
+
     )
 
     time.sleep(10)
@@ -53,6 +90,9 @@ async def handle_with_assistant(message, chat_id):
             run_id=run.id
         )
     print(run_status.status)
+
+
+
     if run_status.status == 'completed':
         messages = client.beta.threads.messages.list(
             thread_id=thread_id
@@ -62,15 +102,34 @@ async def handle_with_assistant(message, chat_id):
         role = msg.role
         content = msg.content[0].text.value
         print(f"{role.capitalize()}: {content}")
-        await bot.send_message(chat_id=message.chat.id, text=content)
+
+        fixed_content = await remove_text_in_brackets(content)
+        fixed_content = re.sub(r"\\\(.*?\\\)", lambda m: eval(m.group(0)[2:-2]), fixed_content)
+
+        # Убираем LaTeX-спецсимволы
+        fixed_content = re.sub(r"\\,|\\text|{|}", "", fixed_content)
+
+        # Убираем лишние символы и пробелы
+        fixed_content =  fixed_content.replace("(", "").replace(")", "").replace("\\", "").strip()
+        await bot.send_message(chat_id=message.chat.id, text=fixed_content)
+        date = message.date.strftime("%Y-%m-%d %H:%M:%S")
+        username = message.from_user.username
+        question = message.text
+        answ = content
+        values_to_write.clear()
+        values_to_write.append(date)
+        values_to_write.append(username)
+        values_to_write.append(question)
+        values_to_write.append(answ)
+        await write_to_excel("output.xlsx", values_to_write)
+        values_to_write.clear()
 
 
 def add_user(chat_id):
-    cursor.execute('SELECT chat_id FROM users WHERE chat_id = ?', (chat_id,))
+
     thread = client.beta.threads.create()
-    if cursor.fetchone() is None:
-        cursor.execute('INSERT INTO users (chat_id, thread) VALUES (?, ?)', (chat_id, thread.id))
-        conn.commit()
+    threads[chat_id] = thread.id
+    print(thread.id)
     return thread.id
 
 
@@ -86,7 +145,8 @@ async def start_command(message: types.Message):
 
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
 async def echo_message(message: types.Message):
-    await handle_with_assistant(message, message.chat.id)
+    if message.text != '/send':
+        await handle_with_assistant(message, message.chat.id)
 
 
 # Запуск бота
